b3dd6bee71b59bd3a0c3c2609827c70ef011d5a6
bendem <bendembd@gmail.com>
2013-09-08 18:18:21 +0200

    Ajout de getters
    + getAdjacentLine
    + getRelativePosition (WIP)
    et de la notice HARDCODE pour les fonctions dont les return sont
    hardcodés

--- libs/core/getter.php
+++ libs/core/getter.php
@@ -2,12 +2,20 @@
 
 namespace Rubs\Core;
 
+\Rubs\Loader::uses('Rubs\Utils\Utils');
+
 /**
  * Code permettant de récupérer des informations sur le cube,
  * ses face, ses blocks
  */
 trait Getter {
 
+	/**
+	 * HARDCODE !
+	 * Retourne la face opposée
+	 * @param  int $face
+	 * @return int
+	 */
 	public function getOppositeFace($face) {
 		switch ($face) {
 			case 0:
@@ -45,6 +53,44 @@ trait Getter {
 	}
 
 	/**
+	 * Retourne le numéro des faces adjacentes en faisant le tour du cube
+	 * (Les faces opposées ne se suivent pas)
+	 * @param  int $face Numéro de la face
+	 * @return array
+	 */
+	public function getRoundedAdjacentsFaces($face) {
+		$adj = $this->getAdjacentsFaces($face);
+
+		for ($i = 0; $i < count($adj) / 2; $i++) {
+			if($adj[$i + 1] == $this->getOppositeFace($adj[$i])) {
+				\Rubs\Utils\Utils::reverse($adj[$i + 1], $adj[$i + 2]);
+			}
+		}
+
+		return $adj;
+	}
+
+	/**
+	 * Retourne la ligne séparant deux faces
+	 * @param  int $face1 Numéro de la face 1
+	 * @param  int $face2 Numéro de la face 2
+	 * @return array 	- (bool)  ``col``    : Si la ligne retournée en une colonne
+	 *         		 	- (int)   ``number`` : Indice de la ligne / colonne
+	 *         		 	- (array) ``face1``  : Ligne de la première face
+	 *         		 	- (array) ``face2``  : Ligne de la deuxième face
+	 */
+	public function getAdjacentLine($face1, $face2) {
+		// TODO : WIP
+		$this->security->canBeAdjacent($face1, $face2);
+		return [
+			'col' => true,
+			'number' => [0, 2][rand(0, 1)],
+			'face1' => ['r', 'r', 'r'],
+			'face2' => ['w', 'w', 'w'],
+		];
+	}
+
+	/**
 	 * Renvoie les blocks adjacents à deux faces
 	 * @param  int $face1 Face 1
 	 * @param  int $face2 Face 2
@@ -53,6 +99,39 @@ trait Getter {
 	 * @todo  implementing it
 	 */
 	public function getAdjacentsBlocks($face1, $face2) {
+	}
+
+	/**
+	 * HARDCODE !
+	 * Retourne la position relative d'une face par rapport à une autre
+	 * @param  int $face1 Première face
+	 * @param  int $face2 Deuxième face
+	 * @return string 'left', 'right', 'top', 'bottom'
+	 */
+	public function getRelativePosition($face1, $face2) {
+		switch ($face1) {
+			case 0:
+				switch ($face2) {
+					case 1: return 'left';
+					case 2: return 'bottom';
+					case 3: return 'right';
+					case 4: return 'top';
+				}
+			case 1:
+				switch ($face2) {
+					case 0: return 'top';
+					case 2: return 'right';
+					case 4: return 'left';
+					case 5: return 'bottom';
+				}
+			case 2:
+				switch ($face2) {
+					case 0: return '';
+					case 1: return 'right';
+					case 3: return 'left';
+					case 5: return 'bottom';
+				}
+		}
 	}
 
 }
