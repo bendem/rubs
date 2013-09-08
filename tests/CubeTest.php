6ee13b2b18cd48c3161eb408d8f431d0b4de28c6
bendem <bendembd@gmail.com>
2013-09-08 21:34:06 +0200

    Test de la méthode getCube et de la génération de cube

--- tests/CubeTest.php
+++ tests/CubeTest.php
@@ -36,9 +36,53 @@ class CubeTest extends PHPUnit_Framework_TestCase {
 		$this->assertContains('<!DOCTYPE html>', $generatedContent);
 	}
 
+	public function testGetCube() {
+		$cube = new Rubs\Cube(true);
+
+		// Accès à une valeur protégée d'une classe
+		$reflectedCube = new ReflectionClass('Rubs\Cube');
+		$p = $reflectedCube->getProperty("cube");
+		$p->setAccessible(true);
+		$generatedCube = $p->getValue($cube);
+
+		$this->assertEquals($generatedCube, $cube->getCube());
+	}
+
+	/**
+	 * @depends testGetCube
+	 */
 	public function testCubeGeneration() {
 		$cube = new Rubs\Cube(true);
-		$this->assertTrue($this->security->isValidCube($cube->getCube()));
+		$cubeContent = $cube->getCube();
+		$this->assertTrue($cube->security->isValidCube($cubeContent));
+		$this->assertEquals(6, count($cubeContent));
+
+		$colorsCount = [];
+		foreach ($cubeContent as $face) {
+			$this->assertEquals(3, count($face));
+
+			foreach ($face as $line) {
+				$this->assertEquals(3, count($line));
+
+				foreach ($line as $block) {
+					if(isset($colorsCount[$block])) {
+						$colorsCount[$block]++;
+					} else {
+						$colorsCount[$block] = 1;
+					}
+				}
+			}
+		}
+
+		$colors = [];
+		foreach ($cube->colors as $color) {
+			$colors[$color] = 9;
+		}
+
+		ksort($colors);
+		ksort($colorsCount);
+
+		$this->assertEquals($colors, $colorsCount);
 	}
 
 }
