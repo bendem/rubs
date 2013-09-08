181b2ee0a32caa3c0af29f61d128cec8aaa4f8ce
bendem <bendembd@gmail.com>
2013-09-08 18:25:34 +0200

    Ajout de tests unitaires
    PARCE QUE YOLO ! :-)

---
+++ tests/CubeTest.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once('../libs/cube.php');
+
+class CubeTest extends PHPUnit_Framework_TestCase {
+
+	public function setUp(){ }
+	public function tearDown(){ }
+
+	public function testOppositeFacesCantBeAdjacent() {
+		$cube = new Rubs\Cube(true);
+		for ($i = 0; $i < 5; $i++) {
+			$error = false;
+			try {
+				$cube->security->canBeAdjacent($i, $cube->getOppositeFace($i));
+			} catch (\Rubs\Exceptions\CantBeAdjacentException $e) {
+				$error = true;
+			}
+			$this->assertTrue($error);
+		}
+	}
+
+	public function testSkipRenderingTrue() {
+		ob_start();
+		$cube = new Rubs\Cube(true);
+		$cube->__destruct();
+		$generatedContent = ob_get_clean();
+		$this->assertEquals('', $generatedContent);
+	}
+
+	public function testSkipRenderingFalse() {
+		ob_start();
+		$cube = new Rubs\Cube();
+		$cube->__destruct();
+		$generatedContent = ob_get_clean();
+		$this->assertContains('<!DOCTYPE html>', $generatedContent);
+	}
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+error_reporting(E_ALL);
+
+define('DS', DIRECTORY_SEPARATOR);
+define('APP', dirname(dirname(__FILE__)));
+
+require APP . DS . 'libs' . DS . 'includes.php';
